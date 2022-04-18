c7252e0f0276aec20143494d7a00e4a5c3368d64
SasiniD <sasidesilvaabc@gmail.com>
2022-04-18 23:56:25 +0530

    Resolved issues after moving service folder

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
-import { ProductService } from '../services/product.service';
+import { ProductService } from 'src/app/services/product.service';
 import { ViewProductDetailsComponent } from '../view-product-details/view-product-details.component';
 
 @Component({
