cffa3d4d712c0c8b4e326f2d2d3e526ff7d400fb
SasiniD <sasidesilvaabc@gmail.com>
2022-04-18 22:19:21 +0530

    Reactive forms implemented

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
@@ -6,11 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
+  productFrom = this.fb.group({
+    productName: ['', Validators.required],
+    description: ['', Validators.required],
+    category: ['', Validators.required],
+    brand : ['', Validators.required],
+    expiredDate: ['', Validators.required],
+    manufacturedDate: ['', Validators.required],
+    batchNumber: ['', Validators.required],
+    unitPrice: ['', [Validators.required, Validators.min(1)]],
+    quantity: ['', [Validators.required, Validators.min(50)]],
+    createdDate: ['', Validators.required],
+  });
 
-  constructor() { }
+  isDataUploading = false;
+
+  constructor(private fb: FormBuilder) {}
   
   ngOnInit(): void {
-    
   }
+
+  get f() {
+    return this.productFrom.controls;
+  }
+
+  onSubmit() {}
 
 }
