57329631bde312fdb641d937df9048112b9d5460
SasiniD <sasidesilvaabc@gmail.com>
2022-04-18 21:54:22 +0530

    reactive forms starting commit

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -8,34 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class AddProductComponent implements OnInit {
 
   constructor() { }
-
+  
   ngOnInit(): void {
-    alert("ngOnInit Called");
-    console.log("triggered ngOnInit")
+    
   }
 
-  ngDoCheck() {
-    console.log("triggered ngDoCheck");
-  }
-
-  ngAfterContentInit() {
-    console.log("triggered ngAfterContentInit");
-  }
-
-  ngAfterContentChecked() {
-    console.log("triggered ngAfterContentChecked");
-  }
-
-  ngAfterViewInit() {
-    console.log("triggered ngAfterViewInit");
-  }
-
-  ngAfterViewChecked() {
-    console.log("triggered ngAfterViewChecked");
-  }
-
-  ngOnDestroy() {
-    alert("ngDestroy Called");
-    console.log("triggered ngOnDestroy");
-  }
 }
