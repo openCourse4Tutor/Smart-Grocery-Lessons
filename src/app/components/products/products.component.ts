97e57463e16c54e54daa129f98ee1cf2a0eab723
SasiniD <sasidesilvaabc@gmail.com>
2022-04-18 21:43:49 +0530

    Data bindings and pipes

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
+  public rowIndex!: number;
   constructor() {}
 
   ngOnInit(): void {}
@@ -17,7 +18,7 @@ export class ProductsComponent implements OnInit {
       createdDate: '2020.01.29',
       quantity: 100,
       unitPrice: '400',
-      productDescription: 'White Basmathi Rice imported from pakistan',
+      productDescription: 'White Basmathi Rice imported from pakistan. High quality rice with extra fragnance. Organically grown.',
     },
     {
       productId: '002',
@@ -44,4 +45,8 @@ export class ProductsComponent implements OnInit {
       productDescription: 'Imported mysoor dhal from India',
     },
   ];
+
+  public selectProduct(selectedRow: number){
+    this.rowIndex = selectedRow;
+    }
 }
